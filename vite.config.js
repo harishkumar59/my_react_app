67ecd087da672f7c9ad490ff6a114979cf345f7c
unknown <harishkumarkallepalli@example.com>
2025-11-08 23:41:02 +0530

    second 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my_react_app/', // 👈 very important: your repo name here
 })
